847b3763dccb48729e0f884ba50ae4afed9a041f
gary <harshprakash000@gmail.com>
2024-05-24 02:33:22 +0530

    Fix : Removed unused console.log

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -5,75 +5,75 @@ import UserModel from "@/models/user.model";
 import bcrypt from "bcryptjs";
 import config from "@/conf/config";
 
-const authOptions : NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text" , placeholder: "Enter your username"},
-                password: {  label: "Password", type: "password" , placeholder: "Enter your password"}
-            },
-            async authorize(credentials : any): Promise<any>{
-                await connectDB();
-                try {
-                    const user = await UserModel.findOne({
-                        $or : [
-                            {
-                                username: credentials.username
-                            }
-                        ]
-                    });
-
-                    console.log(user);
-
-                    if(!user){
-                        throw new Error("No user found")
-                    }
-
-                    console.log(credentials.password);
-                    console.log(user.password);
-
-                    const isValid = await bcrypt.compare(credentials.password, user.password);
-
-                    
-                    console.log(isValid);
-
-                    if(!isValid){
-                        throw new Error("Invalid password");
-                    }
-
-                    return user;
-                    
-                } catch (error: any) {
-                    console.log("Error :: " , error);
-                    throw new Error(error.message);
-                }
-            },
-            
-        })
-    ],
-    callbacks: {
-        async jwt({token , user}){
-            if(user){
-                token._id = user._id;
-                token.username = user.username;
-            }
-            return token;
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Enter your username",
         },
-        async session({session , token}){
-            if(token){
-                session.user._id = token.user._id;
-                session.user.username = token.user.username;
-            }
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await connectDB();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              {
+                username: credentials.username,
+              },
+            ],
+          });
 
-            return session;
+          if (!user) {
+            throw new Error("No user found");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isValid) {
+            throw new Error("Invalid password");
+          }
+
+          return user;
+        } catch (error: any) {
+          throw new Error(error.message);
         }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id;
+        token.username = user.username;
+      }
+      return token;
     },
-    session: {
-        strategy: "jwt",
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token.user._id;
+        session.user.username = token.user.username;
+      }
+
+      return session;
     },
-    secret : config.NEXTAUTH_SECRET
-}
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: config.NEXTAUTH_SECRET,
+};
 
 export default authOptions;
