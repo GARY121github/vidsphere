70d747624f9bdf2cb26106eacd035378daa1fa7e
gary <harshprakash000@gmail.com>
2024-05-26 00:15:55 +0530

    feature: created unique username api

---
+++ src/app/api/v1/user/check-unique-username/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/db/connectDB";
+import UserModel from "@/models/user.model";
+import ApiError from "@/utils/ApiError";
+import ApiResponse from "@/utils/ApiResponse";
+import { userNameSchema } from "@/schemas/signUp.schema";
+
+export async function GET(request: NextRequest) {
+  await connectDB();
+  try {
+    const body = await request.json();
+    const { username } = body;
+
+    const isValidData = userNameSchema.safeParse({ username });
+
+    if (isValidData.success === false) {
+      const errorMessage: string = isValidData.error.errors
+        .map((error: any) => `${error.path.join(".")} ${error.message}`)
+        .join(". ");
+      throw new ApiError(400, "Please provide valid data. " + errorMessage);
+    }
+
+    const user = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (user) {
+      throw new ApiError(400, "Username already exists");
+    }
+
+    return NextResponse.json(new ApiResponse(200, "Username is avaliable"), {
+      status: 200,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      new ApiResponse(
+        error.statusCode || 500,
+        error.message || "Internal Server Error"
+      ),
+      { status: error.statusCode || 500 }
+    );
+  }
+}
