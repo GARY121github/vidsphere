db28ff0aa188e0f3babd112cfad7e3b410e0795a
Falgun Patel <falgunmpatel9123@gmail.com>
2024-06-02 22:15:18 +0530

    chore: update AWS S3 configuration and credentials

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,12 +1,16 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import config from "@/conf/config";
 
+/*
+The SDK automatically detects AWS credentials set as variables in your environment and uses them for SDK requests, eliminating the need to manage credentials in your application. The environment variables that you set to provide your credentials are:
+
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_SESSION_TOKEN
+*/
+
 const s3 = new S3Client({
   region: config.AWS_S3_BUCKET_REGION,
-  credentials: {
-    accessKeyId: config.AWS_ACCESS_KEY,
-    secretAccessKey: config.AWS_SECRET_KEY,
-  },
 });
 
 export default s3;
