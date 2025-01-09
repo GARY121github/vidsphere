95fd6e1464cb1d5fc9d387a3a389daebda2ad22a
Falgun Patel <falgunmpatel9123@gmail.com>
2025-01-09 18:00:27 +0530

    fix: after altering layout for sidebar, landing pages and auth pages were not taking full width.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,26 +6,24 @@ interface LayoutProps {
 
 const layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      <div className="flex flex-col md:flex-row relative items-center z-50">
-        <div className="w-full md:w-3/6">
-          <div
-            className="h-[60vh] md:h-screen bg-cover flex flex-col items-center justify-center"
-            style={{
-              backgroundImage: `url(${"/images/auth.jpg"})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            <p className="text-xl md-text-3xl font-semibold text-center text-orange-700">
-              &nbsp; &nbsp; &nbsp; &nbsp; Welcome to VidSphere{" "}
-            </p>
-          </div>
+    <div className="w-full flex flex-col md:flex-row relative items-center z-50">
+      <div className="w-full md:w-3/6">
+        <div
+          className="h-[60vh] md:h-screen bg-cover flex flex-col items-center justify-center"
+          style={{
+            backgroundImage: `url(${"/images/auth.jpg"})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <p className="text-xl md-text-3xl font-semibold text-center text-orange-700">
+            &nbsp; &nbsp; &nbsp; &nbsp; Welcome to VidSphere{" "}
+          </p>
         </div>
-        <div className="flex items-center justify-center bg-gray-900 md:h-screen w-full md:w-3/6">
-          {children}
-        </div>
+      </div>
+      <div className="flex items-center justify-center bg-gray-900 md:h-screen w-full md:w-3/6">
+        {children}
       </div>
     </div>
   );
