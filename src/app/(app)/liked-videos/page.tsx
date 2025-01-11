e59fbb2264d62b0fc8df661b72e21e4611cceb66
GARY121github <harshprakash000@gmail.com>
2025-01-11 23:36:26 +0530

    Video List Card completed

--- src/app/(app)/liked-videos/page.tsx
+++ src/app/(app)/liked-videos/page.tsx
@@ -18,7 +18,7 @@ export default function LikedVideos() {
     setLoading(true);
     try {
       const response = await axios.get<ApiResponse>("api/v1/like");
-      console.log(response.data.data);
+
       setLikedVideos(response.data.data);
     } catch (error: any) {
       const axiosError = error as AxiosError<ApiError>;
